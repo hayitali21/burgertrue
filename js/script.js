1f73a91e039683370469fd4954a7d60514d32146
hayitali21 <81285826+hayitali21@users.noreply.github.com>
2021-08-16 15:15:06 +0500

    classList add

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 window.addEventListener('load', function(){
     let show = document.querySelector('.show_hide-menu'),
     menu = document.querySelector('.menu'),
-    fa = document.querySelector('.fa-sign-in')
+    fa = document.querySelector('.fa-sign-in'),
+    dx = this.document.querySelector('.d-x')
     console.log(show);
     console.log(menu);
     
     show.addEventListener('click', function(){
-      menu.classList.toggle('lefting');
-      fa.classList.toggle('rotate');
+      menu.classList.add('lefting');
+      setTimeout(function(){
+          dx.classList.add('dblock');
+      }, 700)
+
+      
+    })
+
+    dx.addEventListener('click', function(){
+      menu.classList.remove('lefting');
+      dx.classList.remove('dblock');
     })
   })
